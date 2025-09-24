91cda3d31d9ce61a28a672f39b81b32358d4e15d
Emediong <emediongjonah66@gmail.com>
2025-09-24 04:05:42 +0100

    for deployment

--- cosmetic-app/src/App.jsx
+++ cosmetic-app/src/App.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 import Header from "./components/header";
 import NavBar from "./components/navigation";
 import Hero from "./components/hero";
@@ -9,13 +11,54 @@ import Footer from "./components/footer";
 import productData from "./productData";
 function App() {
   const productsElement = productData.map((product) => {
-    return <Product key={product.id} {...product} />;
+    return <Product key={product.id} {...product} id={product.id} />;
   });
+
+  const [choice, setChoice] = useState(false);
+
+  function toggleLiked() {
+    setChoice((prevStatus) => !prevStatus);
+  }
+
+  const [search, setSearch] = useState(false);
+
+  function toggleSearch() {
+    setSearch((prev) => !prev);
+  }
+
+  function productSearch(searchTerm) {
+  const lower = searchTerm.currentTarget.value.toLowerCase()
+    const number = searchTerm.currentTarget.value
+ 
+
+  const filterdProducts = productData.filter(products => products.productName.toLowerCase().includes(lower) ||  products.category.includes(lower) || products.price.includes(number))
+
+  }
+
   return (
     <>
       <Header />
-      <NavBar />
-      <Hero />
+      <NavBar
+        choice={choice}
+        toggleLiked={toggleLiked}
+        toggleSearch={toggleSearch}
+      />
+
+      <div className="relative">
+        <Hero page={"Shop"} />
+        <div className=" absolute top-15 left-128  z-10">
+          {search ? (
+            <input
+              type="search"
+              className="px-3 py-2 rounded text-black border-solid border-2 bg-white w-full"
+              placeholder="Search for products"
+              onChange={productSearch}
+            />
+          ) : (
+            ""
+          )}
+        </div>
+      </div>
       <Content />
       <div className="flex items-stretch justify-center">
         <Filter />
--- cosmetic-app/src/components/products.jsx
+++ cosmetic-app/src/components/products.jsx
@@ -1,52 +1,70 @@
+import { useState } from "react";
+
 import { FaStar } from "react-icons/fa";
 import { FaCartPlus } from "react-icons/fa";
 import { BiPurchaseTagAlt } from "react-icons/bi";
 
-function Product({
-  img,
-  productName,
-  price,
-  oldPrice,
-  rating,
-  category,
-  discount,
-}) {
+function Product(prop) {
+  const [cart, setCart] = useState([]);
+
+  function addToCart(item) {
+    setCart((prev) => {
+      const updated = [prev, item];
+      return updated;
+    });
+  }
+
   return (
     <div>
-      <div className="relative w-auto h-67">
-        <img src={img} alt="" className="w-full h-full rounded-3xl" />
-        <div className="absolute top-5 left-5 bg-green-800 px-3 rounded-full text-white text-sm">
-          {discount}% Off
+      <div className="relative w-auto h-72">
+        <img src={prop.img} alt="" className="w-full h-full rounded-3xl" />
+        <div className="absolute top-5 left-5 bg-green-800 px-3 rounded-full text-white text-base md:text-xl">
+          {prop.discount}% Off
         </div>
       </div>
       <div className="py-3 w-auto leading-loose bg-white px-4">
         <div className="flex justify-between items-center mx-7">
-          <p>{category.charAt(0).toUpperCase() + category.slice(1)}</p>
+          <p>
+            {prop.category
+              ? prop.category.charAt(0).toUpperCase() + prop.category.slice(1)
+              : ""}
+          </p>
           <p className="text-amber-300 flex">
             {" "}
             <FaStar />
-            <span className="text-black">{rating}</span>
+            <span className="text-black">{prop.rating}</span>
           </p>
         </div>
         <div className="font-semibold">
           <p className="text-[1.3rem]">
-            {productName.charAt(0).toUpperCase() + productName.slice(1)}
+            {prop.productName.charAt(0).toUpperCase() +
+              prop.productName.slice(1)}
           </p>
         </div>
         <div className="flex items-start">
           <p className="text-orange-400 mr-6 text-base font-semibold">
-            ${price}
+            ${prop.price}
           </p>
           <p className="text-stone-600  text-base font-semibold">
-            $<del>{oldPrice}</del>
+            $<del>{prop.oldPrice}</del>
           </p>
         </div>
-        <div className="flex justify-evenly items-center">
+
+        <div className="flex justify-evenly items-center flex-wrap gap-4">
           <div>
-            <button className="bg-green-700 px-3 text-white rounded-full hover:opacity-85 flex items-center gap-2"><BiPurchaseTagAlt />Purchase</button>
+            <button className="bg-green-700 px-3 text-white rounded-full hover:opacity-85 flex items-center gap-2">
+              <BiPurchaseTagAlt />
+              Purchase
+            </button>
           </div>
           <div>
-            <button className="border-green-700 border-solid flex items-center gap-2 border-2 px-2 rounded-full hover:bg-green-500 hover:text-white hover:opacity-85"><FaCartPlus />Add to Cart</button>
+            <button
+              className="border-green-700 border-solid flex items-center gap-2 border-2 px-2 rounded-full hover:bg-green-500 hover:text-white hover:opacity-85 transition-colors duration-300"
+              onClick={() => addToCart(prop)}
+            >
+              <FaCartPlus />
+              Add to Cart
+            </button>
           </div>
         </div>
       </div>
